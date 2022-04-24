52f590d6cf1bd9528fafe71e49ede79dbb28c5e8
Stephen Jakku <uttamjakku96@gmail.com>
2022-04-23 22:02:49 -0700

    js(phoenix): setup firebase auth
    - firebase integration
    - add firebase config
    - add protected routes
    - add context file
    - update _app for the firebase integration

--- js/phoenix/pages/_app.js
+++ js/phoenix/pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { AuthContextProvider } from "../context/AuthContext";
+import { useRouter } from "next/router";
+import ProtectedRoute from "../components/ProtectedRoutes";
+
+const noAuthRequired = ["/", "/login", "/signup"];
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <AuthContextProvider>
+      {noAuthRequired.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ) : (
+        <ProtectedRoute>
+          <Component {...pageProps} />
+        </ProtectedRoute>
+      )}
+    </AuthContextProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
