a209f269891bcfdf851884c668a4ff6dde764924
AbhiGattineni <abhishekgattineni@gmail.com>
2022-04-27 23:19:37 -0700

    add signup page created

--- js/phoenix/src/containers/Auth/Login/LoginForm.jsx
+++ js/phoenix/src/containers/Auth/Login/LoginForm.jsx
@@ -1,12 +1,12 @@
 import { Input, Button } from "../../../components"
 
 export const LoginForm = () => {
-  return(
-    <form className="w-full max-w-sm mt-6 flex flex-col justify-center">
+  return (
+    <form className="max-w-sm mt-6 flex flex-col justify-center">
       <Input placeholder="Email" />
       <Input placeholder="Password" />
       <div className="flex justify-center">
-        <Button text="Sign in"/>
+        <Button text="Sign in" />
       </div>
     </form>
   )
--- js/phoenix/src/containers/index.js
+++ js/phoenix/src/containers/index.js
@@ -1,1 +1,2 @@
 export { LoginForm } from "./Auth/Login/LoginForm"
+export { SignupForm } from "./Auth/Signup/SignupForm"
