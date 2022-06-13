d18967fee5d2889d63867c4d9e188d5a1eb1c6d8
AbhiGattineni <abhishekgattineni@gmail.com>
2022-06-13 16:14:50 -0700

    models for funds created

--- python/utopia/utopia/funds/models.py
+++ python/utopia/utopia/funds/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Fund(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Date(models.Model):
+    date = models.DateField
+
+
+class Nav(models.Model):
+    fund_id = models.ForeignKey(Fund, on_delete=models.CASCADE)
+    date_id = models.ForeignKey(Date, on_delete=models.CASCADE)
+    nav = models.DecimalField(max_digits=8, decimal_places=4)
